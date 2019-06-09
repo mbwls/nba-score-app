c061790a41e900562ab9ae0f854bc0ba227e4226
Matthew Bowles <mbowles@kenna.ca>
2019-06-09 13:29:05 -0400

    Initialized Node server

---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+require('dotenv').config({path:process.env.ENV_FILE});
+
+const express = require('express');
+const app = express();
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Node server started on ${process.env.PORT}`);
+})
