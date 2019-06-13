823111d5cdc9e9b7e80ca0a3ccf21d57d8db30a9
Matthew Bowles <mbowles@kenna.ca>
2019-06-12 22:12:54 -0400

    Updated routes; changed favicon

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,8 @@
-require('dotenv').config({path:process.env.ENV_FILE});
+require('dotenv').config({ path: process.env.ENV_FILE });
 
 const express = require('express');
 const app = express();
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Node server started on ${process.env.PORT}`);
-})
+});
--- server/src/controllers/sports-feed.controller.js
+++ server/src/controllers/sports-feed.controller.js
@@ -1,1 +1,6 @@
+const { errorResponse, successResponse } = require('../services/util.service');
 const SportsFeedService = require('../services/sports-feed.service');
+
+const getTodayScores = async (req, res) => {};
+
+const getGameData = async (req, res) => {};
