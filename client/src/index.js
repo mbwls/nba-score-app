3bc35e349a0223f9620cfc94baa13fb205244c6d
Matthew Bowles <mbowles@kenna.ca>
2019-07-07 09:36:04 -0400

    Updated reference in React index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './app';
+import App from './app.js';
 import configureStore from './store';
 
 const store = configureStore();
