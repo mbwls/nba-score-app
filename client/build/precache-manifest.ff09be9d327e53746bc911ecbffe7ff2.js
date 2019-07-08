6eda13569ddf8380ed46edb3f2aaf62398e91b24
Matthew Bowles <mbowles@kenna.ca>
2019-07-08 17:59:10 -0400

    Removed React build from .gitignore

---
+++ client/build/precache-manifest.ff09be9d327e53746bc911ecbffe7ff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df917df05b4e29b9981d4d7eb74a6e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dade376c6757f0060b8",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "b4a582306bf66e462eef",
+    "url": "/static/css/main.2461a1d4.chunk.css"
+  },
+  {
+    "revision": "1dade376c6757f0060b8",
+    "url": "/static/js/2.0ae78b1d.chunk.js"
+  },
+  {
+    "revision": "b4a582306bf66e462eef",
+    "url": "/static/js/main.3a417231.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56af72775bce4a27636b4e7cbb1f025c",
+    "url": "/static/media/btn_close.56af7277.svg"
+  }
+]);
