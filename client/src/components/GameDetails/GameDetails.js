360c1820e3b43c0f2c4b2210c256463a39218086
Matthew Bowles <mbowles@kenna.ca>
2019-08-14 18:41:04 -0400

    Initial box score layout

--- client/src/components/GameDetails/GameDetails.js
+++ client/src/components/GameDetails/GameDetails.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './GameDetails.css';
 import DetailsHeader from './DetailsHeader/DetailsHeader';
 import TeamStats from './TeamStats/TeamStats';
+import BoxScore from './BoxScore/BoxScore';
 
 const GameDetails = props => {
     return (
         <div className='game-details'>
             <DetailsHeader {...props} />
+            <BoxScore {...props} />
             <TeamStats {...props} />
         </div>
     );
