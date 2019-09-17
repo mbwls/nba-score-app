b5ccf183160efa7c149af9af18799b437324e76a
Matthew Bowles <matto.bowles@gmail.com>
2019-09-16 22:28:15 -0400

    Revert "Added resize event to viewport unit workaround"
    This reverts commit 3d6c31f20ce5e6b824ab4d6a74d143c54d4b8cbe.

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,9 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 
-/* CUSTOM CSS VH VARIABLE FOR MOBILE BROWSWER WORKAROUND */
+const store = configureStore();
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-// We listen to the resize event (relevant for portrait/landscape rotations)
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
-const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
