77be93f2b3832fa4800654b93beb6c8d93b75566
Matthew Bowles <matto.bowles@gmail.com>
2019-09-24 17:59:16 -0400

    Closed active game when switching via datepicker on mobile

--- client/src/components/DateNav/DateSelect/DateSelect.js
+++ client/src/components/DateNav/DateSelect/DateSelect.js
@@ -1,18 +1,40 @@
 import { DatePicker } from 'antd';
 import React from 'react';
 import moment from 'moment';
+import Media from 'react-media';
 import '../DateNav.css';
 
 const DateSelect = props => {
     return (
-        <DatePicker
-            defaultValue={moment(props.dateKey).clone()}
-            className='date-btn'
-            placeholder=''
-            onChange={(date, dateString) => {
-                props.setScoresDate(dateString);
-            }}
-        />
+        <Media queries={{
+            small: '(max-width: 820px)',
+            large: '(min-width: 821px)'
+        }}>
+            {matches => (
+                <React.Fragment>
+                    {matches.small &&
+                        <DatePicker
+                            defaultValue={moment(props.dateKey).clone()}
+                            className='date-btn'
+                            placeholder=''
+                            onChange={(date, dateString) => {
+                                props.setSelectedGame({});
+                                props.setScoresDate(dateString);
+                            }}
+                        />}
+
+                    {matches.large &&
+                        <DatePicker
+                            defaultValue={moment(props.dateKey).clone()}
+                            className='date-btn'
+                            placeholder=''
+                            onChange={(date, dateString) => {
+                                props.setScoresDate(dateString);
+                            }}
+                        />}
+                </React.Fragment>
+            )}
+        </Media>
     )
 }
 
